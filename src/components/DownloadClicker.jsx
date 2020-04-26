b2bb2b329f27e0b3ad0e3b7d83ca84abaa44e8b5
Konsta Purtsi <kovipu@utu.fi>
2020-04-26 17:30:47 +0300

    Add running logic.

--- src/components/DownloadClicker.jsx
+++ src/components/DownloadClicker.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const heloust = new Audio('heloust.ogg')
+
 export default function DownloadClicker() {
+  const [runners, setRunners] = useState([]);
+
+  function handleClick() {
+    // TODO: clear these, this will crash the browser at some point...
+    setRunners([...runners, true]);
+    heloust.play();
+  }
+
   return (
     <Wrapper>
       <RunnerWrapper>
-        <Rami src="rami_jamit2_transparent.gif" left={0} />
-        <Rami src="rami_jamit_transparent.gif" left={-40} />
+        <Rami src="rami_jamit2_transparent.gif" />
+        <Rami src="rami_jamit_transparent.gif" left={20}/>
+        {runners.map(() => (
+          <Runner src="rami_jamit_transparent.gif"/>
+        ))}
+        <Computer src="ramin_kone.png" />
       </RunnerWrapper>
-      <Button>download</Button>
+      <Button onClick={handleClick}>
+        download
+      </Button>
     </Wrapper>
   );
 }
@@ -22,12 +38,43 @@ const Wrapper = styled.div`
 
 const RunnerWrapper = styled.div`
   width: 100%;
+  height: 100px;
+  position: relative;
 `;
 
 const Rami = styled.img`
-  position: relative;
+  position: absolute;
   height: 64px;
-  left: ${props => props.left}px;
+  margin-top: 10px;
+  float: left;
+  margin-left: ${props => props.left}px;
+`;
+
+const Runner = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 64px;
+  margin-top: 10px;
+  -webkit-animation: slide 1s backwards;
+  animation: slide 1s backwards;
+
+  @-webkit-keyframes slide {
+    100% { left: 330px; }
+  }
+
+  @keyframes slide {
+    100% { left: 330px; }
+  }
+`;
+
+const Computer = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 80px;
+  float: right;
+  z-index: 1000;
 `;
 
 const Button = styled.button`
@@ -39,4 +86,5 @@ const Button = styled.button`
   font-size: 14px;
   font-weight: 700;
   text-transform: uppercase;
+  margin-top: 1em;
 `;
